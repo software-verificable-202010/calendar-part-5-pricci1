8b7c480db26da455b425d3577858f68881a04188
pricci1 <pricci1@miuandes.cl>
2020-04-11 22:21:08 -0400

    feat: MonthGrid View and ViewModel

---
+++ SVCalendar.WPF/View/MonthGridViewModel.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Windows.Media;
+using SVCalendar.WPF.Annotations;
+
+namespace SVCalendar.WPF.View
+{
+    class MonthGridViewModel : INotifyPropertyChanged
+    {
+
+        public MonthGridViewModel()
+        {
+            CurrentDate = DateTime.Today;
+            MonthDaysList = InitializeDays();
+            NextMonthCommand = new RelayCommand(OnNextMonth);
+            PreviousMonthCommand = new RelayCommand(OnPreviousMonth);
+        }
+
+        private void OnPreviousMonth()
+        {
+            CurrentDate = CurrentDate.AddMonths(-1);
+            MonthDaysList = InitializeDays();
+        }
+
+        private void OnNextMonth()
+        {
+            CurrentDate = CurrentDate.AddMonths(1);
+            MonthDaysList = InitializeDays();
+        }
+
+        private List<DayBlock> _monthDaysList;
+        public List<DayBlock> MonthDaysList
+        {
+            get => _monthDaysList;
+            set
+            {
+                _monthDaysList = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private DateTime _currentDate;
+        public DateTime CurrentDate
+        {
+            get => _currentDate;
+            private set
+            {
+                _currentDate = value;
+                OnPropertyChanged();
+                MonthYearString = $"{_monthNames[value.Month - 1]} {value.Year}";
+            }
+        }
+
+        public String MonthYearString
+        {
+            get => _monthYearString;
+            private set
+            {
+                _monthYearString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public RelayCommand NextMonthCommand { get; private set; }
+
+        public RelayCommand PreviousMonthCommand { get; private set; }
+
+        public List<DayBlock> InitializeDays()
+        {
+            var monthDaysList = new List<DayBlock>();
+            int daysInCurrentMonth = DateTime.DaysInMonth(CurrentDate.Year, CurrentDate.Month);
+            int firstWeekDayOfCurrentMonth = (int)CurrentDate.AddDays(-1 * CurrentDate.Day + 1).DayOfWeek;
+            // Make 0 = Monday, ... 6 = Sunday
+            int adjustedFirstWeekDayOfCurrentMonth =
+                firstWeekDayOfCurrentMonth == 0 ? 6 : firstWeekDayOfCurrentMonth - 1;
+
+            for (int i = 1; i < adjustedFirstWeekDayOfCurrentMonth + 1; i++)
+            {
+                monthDaysList.Add(new DayBlock(-1, -1));
+            }
+
+            for (int i = 1; i <= daysInCurrentMonth; i++)
+            {
+                int weekDayNumber = Math.Abs(7 - adjustedFirstWeekDayOfCurrentMonth + i) % 7;
+                monthDaysList.Add(new DayBlock(i, weekDayNumber));
+            }
+
+            return monthDaysList;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private readonly string[] _monthNames = {
+            "January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
+            "November", "December"
+        };
+
+        private string _monthYearString;
+    }
+
+    internal class DayBlock
+    {
+        public DayBlock(int dayNumber, int weekDayNumber)
+        {
+            DayNumber = dayNumber;
+            WeekDayNumber = weekDayNumber;
+        }
+
+        public int DayNumber { get; set; }
+        public String DayNumberString => DayNumber > 0 ? DayNumber.ToString() : "";
+        public int WeekDayNumber { get; set; }
+        public SolidColorBrush Color =>
+            WeekDayNumber.Equals(5) || WeekDayNumber.Equals(6) ? Brushes.LightCoral : Brushes.LightBlue;
+    }
+}
