bac903cb8fe7cb91d06d3f9292a605cf55133ae4
Pedro <pricci1@miuandes.cl>
2020-05-06 02:56:04 -0400

    feat(view): weekly view as columns

--- SVCalendar.WPF/MainWindowViewModel.cs
+++ SVCalendar.WPF/MainWindowViewModel.cs
@@ -1,10 +1,47 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SVCalendar.WPF.View;
 
 namespace SVCalendar.WPF
 {
-    class MainWindowViewModel
+    class MainWindowViewModel : BindableBase
     {
+        public MainWindowViewModel()
+        {
+            CurrentViewModel = _monthGridViewModel;
+            ChangeCalendarModeCommand = new RelayCommand<CalendarMode>(OnChangeCalendarMode);
+        }
+        private readonly MonthGridViewModel _monthGridViewModel = new MonthGridViewModel();
+        private readonly WeekGridViewModel _weekGridViewModel = new WeekGridViewModel();
+
+        public RelayCommand<CalendarMode> ChangeCalendarModeCommand { get; private set; }
+
+
+        private BindableBase _currentViewModel;
+        public BindableBase CurrentViewModel
+        {
+            get => _currentViewModel;
+            set => SetProperty(ref _currentViewModel, value);
+        }
+
+        private void OnChangeCalendarMode(CalendarMode mode)
+        {
+            switch (mode)
+            {
+                case CalendarMode.Monthly:
+                    CurrentViewModel = _monthGridViewModel;
+                    break;
+                case CalendarMode.Weekly:
+                    CurrentViewModel = _weekGridViewModel;
+                    break;
+            }
+        }
+    }
+
+    enum CalendarMode
+    {
+        Monthly,
+        Weekly
     }
 }
---
+++ SVCalendar.WPF/View/WeekGridViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SVCalendar.WPF.View
+{
+    class WeekGridViewModel : BindableBase
+    {
+        public WeekGridViewModel()
+        {
+            WeekDays = new List<WeekDay>
+            {
+                new WeekDay("Monday", "4"),
+                new WeekDay("Tuesday", "5"),
+                new WeekDay("Wednesday", "6"),
+                new WeekDay("Thursday", "7"),
+                new WeekDay("Friday", "8"),
+                new WeekDay("Saturday", "9"),
+                new WeekDay("Sunday", "10")
+            };
+        }
+        public List<WeekDay> WeekDays { get; set; }
+
+    }
+
+    class WeekDay
+    {
+        public WeekDay(string dayName, string dayNumber)
+        {
+            DayName = dayName;
+            DayNumber = dayNumber;
+            HalfHours = new List<string>
+            {
+                " "," "," "," "," "," "," "," "," "," ",
+                " "," "," "," "," "," "," "," "," "," ",
+                " "," "," "," "
+            };
+        }
+
+        public String DayName { get; set; }
+        public String DayNumber { get; set; }
+
+        public List<String> HalfHours { get; set; }
+
+    }
+}
