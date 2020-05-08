4377ba125b5fca21dcaa0723b37c5284ae48b21a
Pedro <pricci1@miuandes.cl>
2020-05-07 22:32:39 -0400

    feat: add EventsRepository

---
+++ SVCalendar.WPF/EventsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SVCalendar.Model;
+
+namespace SVCalendar.WPF
+{
+    class EventsRepository : IEventsRepository
+    {
+        private CalendarDbContext _db;
+
+        public EventsRepository()
+        {
+            _db = new CalendarDbContext();
+        }
+
+        public List<Event> GetEvents() => _db.Events.ToList();
+
+        public void AddEvent(Event eventToAdd)
+        {
+            _db.Add(eventToAdd);
+            _db.SaveChanges();
+        }
+
+    }
+}
---
+++ SVCalendar.WPF/IEventsRepository.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SVCalendar.Model;
+
+namespace SVCalendar.WPF
+{
+    public interface IEventsRepository
+    {
+        List<Event> GetEvents();
+        void AddEvent(Event eventToAdd);
+    }
+}
